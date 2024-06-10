911c28353265601e835a77cba42b5c68fe33832d
Eyerusalem-yirga <156807301+Eyerusalem-yirga@users.noreply.github.com>
2024-06-11 00:07:46 +0300

    first commit

---
+++ functions/index.js
@@ -0,0 +1,45 @@
+const { onRequest } = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+app.use(cors({ origin: true }));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "sucess",
+  });
+});
+// remove 20-26
+app.post("/payment/create", async (req, res) => {
+  const total = req.query.total;
+  if (total > 0) {
+    console.log("payment receieved", total);
+    res.send(total);
+  }
+});
+
+app.post("/payment/create", async (req, res) => {
+  const total = parseInt(req.query.total);
+  if (total > 0) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+    res.status(201).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } else {
+    res.status(403).json({
+      message: "total must be  greater than 0",
+    });
+  }
+});
+
+exports.api = onRequest(app);
